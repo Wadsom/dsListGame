8534053b9bf1ca8152dd5a4298519a31acc11339
Wadson <pinheirowadson@gmail.com>
2024-02-29 15:46:17 -0300

    Game Entity

---
+++ src/test/java/com/wsCorp/dsGameList/DsGameListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wsCorp.dsGameList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsGameListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
